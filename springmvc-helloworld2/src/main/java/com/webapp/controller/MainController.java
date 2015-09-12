e2e75526ba2c004c627860b381e648c9e2e79a20
gmartins <ggmartins@gmail.com>
2015-09-12 13:24:07 -0400

    Spring MVC HelloWorld 4.2

---
+++ springmvc-helloworld2/src/main/java/com/webapp/controller/MainController.java
@@ -0,0 +1,25 @@
+package com.webapp.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+ 
+
+@Controller
+@RequestMapping("/")
+public class MainController {
+	
+    @RequestMapping(method = RequestMethod.GET)
+    public String sayHello(ModelMap model) {
+        model.addAttribute("message", "hello Spring 4 MVC (/)");
+        return "view1";
+    }
+ 
+    @RequestMapping(value="/webapp", method = RequestMethod.GET)
+    public String sayHelloAgain(ModelMap model) {
+        model.addAttribute("message", "hello Spring 4 MVC (webapp)");
+        return "view2";
+    }
+
+}
